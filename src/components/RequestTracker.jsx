7c6b105cd462c454431a368c8846ecdbb66b0627
mujanati13 <simojanati.1j@gmail.com>
2025-07-03 16:08:45 +0100

    c

--- src/components/RequestTracker.jsx
+++ src/components/RequestTracker.jsx
@@ -620,7 +620,7 @@ const RequestTracker = () => {
         component={Paper}
         sx={{ 
           bgcolor: "#fff", 
-          height: "calc(100vh - 95px)", 
+          height: "calc(100vh - 90px)", 
           overflow: "auto",
           border: "1px solid #e0e0e0"
         }}
